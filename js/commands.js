d812859fefd015e47ca8c8b8d209a4b8b3c7d0b0
Yugveer Singh <yugveersingh06@gmail.com>
2023-12-23 16:08:23 +0530

    Updated remove command to accept more arguments

--- js/commands.js
+++ js/commands.js
@@ -1,249 +1,338 @@
 // commands.js
 
 const punish_type = (type) => {
-    if (["+2", "+", "2"].includes(type)) {
-        return PLUS2;
-    } else if (["DNF", "dnf", "D", "d"].includes(type)) {
-        return DNF;
-    } else if ([null, " "].includes(type)) {
-        return null;
-    } else {
-        return null;
-    }
-}
+	if (["+2", "+", "2"].includes(type)) {
+		return PLUS2;
+	} else if (["DNF", "dnf", "D", "d"].includes(type)) {
+		return DNF;
+	} else if ([null, " "].includes(type)) {
+		return null;
+	} else {
+		return null;
+	}
+};
 
 const $cube_commands = {
-    // set cube
-    "cube": (cube) => {
-        if (Object.keys(command_cubes).includes(cube)) {
-            cubeSelect.value = cubes.indexOf(command_cubes[cube]);
-            setScramble().then(((result)=>{ sc = result }));
-            return [`Cube selected to ${command_cubes[cube]}!`, GOOD];
-        } else {
-            if (cube==undefined) { return [`Invalid Cube`, BAD]; }
-            return [`Invalid Cube: ${cube}`, BAD];
-        }
-    },
+	// set cube
+	cube: (cube) => {
+		if (Object.keys(command_cubes).includes(cube)) {
+			cubeSelect.value = cubes.indexOf(command_cubes[cube]);
+			setScramble().then((result) => {
+				sc = result;
+			});
+			return [`Cube selected to ${command_cubes[cube]}!`, GOOD];
+		} else {
+			if (cube == undefined) {
+				return [`Invalid Cube`, BAD];
+			}
+			return [`Invalid Cube: ${cube}`, BAD];
+		}
+	},
 
-    // next scramble
-    "next": () => { setScramble().then(((result)=>{ sc = result; })) },
+	// next scramble
+	next: () => {
+		setScramble().then((result) => {
+			sc = result;
+		});
+	},
 
-    // punish
-    "punish": (type=null) => {
-        return punish(punish_type(type));
-    },
+	// punish
+	punish: (type = null) => {
+		return punish(punish_type(type));
+	},
 
-    // view time log
-    "view": (idx) => {
-        if (isIdxValid(idx)) { ShowTimeModifyDialog(idx-1); }
-        else { return [`Invalid index: ${idx}`, BAD]; }
-    },
+	// view time log
+	view: (idx) => {
+		if (isIdxValid(idx)) {
+			ShowTimeModifyDialog(idx - 1);
+		} else {
+			return [`Invalid index: ${idx}`, BAD];
+		}
+	},
 
-    // edit time log
-    "edit": {
-        "punish": (idx, type) => {
-            if (isIdxValid(idx)) { return edit_punish(idx-1, punish_type(type)); }
-            else { return [`Invalid index: ${idx}`, BAD]; }
-        },
-    },
-    // remove time
-    "remove": (idx) => {
-        if (isIdxValid(idx)) {
-            all_times.splice(idx-1, 1);
-            logs();
-            return [`Removed time on index ${idx}`, PROB];
-        } else {
-            return [`Invalid index: ${idx}`, BAD];
-        }
-    },
+	// edit time log
+	edit: {
+		punish: (idx, type) => {
+			if (isIdxValid(idx)) {
+				return edit_punish(idx - 1, punish_type(type));
+			} else {
+				return [`Invalid index: ${idx}`, BAD];
+			}
+		},
+	},
+	// remove time
+	rm: "remove",
+	remove: (idx) => {
+		const rangeRegex = /^(\d+)-(\d+)$/;
 
-    // copy
-    "copy": {
-        null: () => { return [`Copy what?`, PROB] },
-        "sc": "scramble",
-        "scramble": () => {
-            copy(scramble.value);
-            return [`Copied Scramble!`, GOOD];
-        },
-        "time": () => {
-            copy(timer.value || "0:00.000")
-            return [`Copied Solve Time!`, GOOD];
-        },
-    },
+		const isValidRange = (start, end) => {
+			return isIdxValid(start) && isIdxValid(end);
+		};
 
-    // session controls
-    "session": {
-        // change current session
-        null: "change",
-        "=": "change",
-        "change": (name) => {
-            if (!(s_idxs.includes(name))) {
-                return [`Invalid Session: ${name}`, BAD];
-            }
+		const removeRange = (start, end) => {
+			all_times.splice(start - 1, end - start + 1);
+			logs();
+			return [`Removed elements from index ${start} to ${end}`, GOOD];
+		};
 
-            current_session = s_idxs.indexOf(name);
-            sessionSelect.value = current_session;
-            localStorage.setItem("current_session", current_session);
+		const removeSingleIndex = (index) => {
+			all_times.splice(index - 1, 1);
+			logs();
+			return [`Removed time on index ${index}`, GOOD];
+		};
 
-            all_times = getSession();
+		const removeNegativeIndex = (index) => {
+			const positiveIndex = index * -1;
+			if (isIdxValid(positiveIndex)) {
+				all_times.splice(all_times.length + index, 1);
+				logs();
+				return [`Removed time on index ${index} (${all_times.length + 2 + index})`, GOOD];
+			} else {
+				return [`Invalid index: ${idx}`, BAD];
+			}
+		};
 
-            logs();
+		// handle no arguments
+		if (!idx) {
+			return ["Remove what?", PROB];
+		}
 
-            return [`Session "${name}" Selected`, GOOD];
-        },
+		// check if the input is to remove all elements
+		if (idx.toLowerCase() === "all" || idx === "*") {
+			const confirmation = confirm("Are you sure you want to remove all elements?");
+			if (confirmation) {
+				all_times = [];
+				logs();
+				return ["Removed all elements", GOOD];
+			} else {
+				return ["Operation canceled", BAD];
+			}
+		}
 
-        // add new session
-        "+": "add",
-        "add": (name=null) => {
-            if (name=null) { return [`Session name?`, PROB]; }
-            if (s_idxs.includes(name)) {
-                return [`Session "${name}" exist`, BAD];
-            }
-            sessions[name] = [];
-            s_idxs.push(name);
+		// check if the input is a range
+		const rangeMatch = idx.toString().match(rangeRegex);
+		if (rangeMatch) {
+			const start = parseInt(rangeMatch[1]);
+			const end = parseInt(rangeMatch[2]);
+			return isValidRange(start, end) ? removeRange(start, end) : [`Invalid range: ${idx}`, BAD];
+		}
 
-            // session select
-            s_length = s_idxs.length;
-            sessionSelect.innerHTML = "";
-            createSelect(s_length, s_idxs, sessionSelect,
-                (i, opt) => { return i==(current_session) });
+		// check if the input is a negative index
+		const index = parseInt(idx);
+		if (!isNaN(index) && index < 0) {
+			return removeNegativeIndex(index);
+		}
 
-            logs();
+		// check if the input is a single index
+		if (!isNaN(index) && isIdxValid(index)) {
+			return removeSingleIndex(index);
+		} else {
+			return [`Invalid index: ${idx}`, BAD];
+		}
+	},
 
-            return [`Added new session: ${name}`, GOOD];
-        },
+	// copy
+	copy: {
+		null: () => {
+			return [`Copy what?`, PROB];
+		},
+		sc: "scramble",
+		scramble: () => {
+			copy(scramble.value);
+			return [`Copied Scramble!`, GOOD];
+		},
+		time: () => {
+			copy(timer.value || "0:00.000");
+			return [`Copied Solve Time!`, GOOD];
+		},
+	},
 
-        // remove a session
-        "-": "remove",
-        "remove": (name) => {
-            if (name=null) { return [`Session name?`, PROB]; }
-            if (!(s_idxs.includes(name))) {
-                return [`Session "${name}" not found`, BAD];
-            }
-            if (s_idxs.length <= 1) {
-                return [`There has to be AT LEAST one session!`, PROB];
-            }
+	// session controls
+	session: {
+		// change current session
+		null: "change",
+		"=": "change",
+		change: (name) => {
+			if (!s_idxs.includes(name)) {
+				return [`Invalid Session: ${name}`, BAD];
+			}
 
-            current_session = 0;
-            all_times = getSession();
+			current_session = s_idxs.indexOf(name);
+			sessionSelect.value = current_session;
+			localStorage.setItem("current_session", current_session);
 
-            delete sessions[name];
-            s_idxs[s_idxs.indexOf(name)] = null;
-            s_idxs = s_idxs.filter(i => i);
+			all_times = getSession();
 
+			logs();
 
-            // session select
-            s_length = s_idxs.length;
-            sessionSelect.innerHTML = "";
-            createSelect(s_length, s_idxs, sessionSelect,
-                (i, opt) => { return i==(current_session) });
+			return [`Session "${name}" Selected`, GOOD];
+		},
 
-            logs();
+		// add new session
+		"+": "add",
+		add: (name = null) => {
+			if ((name = null)) {
+				return [`Session name?`, PROB];
+			}
+			if (s_idxs.includes(name)) {
+				return [`Session "${name}" exist`, BAD];
+			}
+			sessions[name] = [];
+			s_idxs.push(name);
 
-            return [`Removed session: ${name}`, PROB];
-        },
+			// session select
+			s_length = s_idxs.length;
+			sessionSelect.innerHTML = "";
+			createSelect(s_length, s_idxs, sessionSelect, (i, opt) => {
+				return i == current_session;
+			});
 
-        // rename a session
-        // TODO
-    },
+			logs();
 
-    // export session
-    "export": () => {
-        var now = new Date();
-        var time = `${now.getFullYear()}${now.getMonth()+1}${now.getDate()}_` +
-                  `${now.getHours()}${now.getMinutes()}${now.getSeconds()}`;
+			return [`Added new session: ${name}`, GOOD];
+		},
 
-        download("application/json", JSON.stringify(sessions), `sessions_${time}`);
+		// remove a session
+		"-": "remove",
+		remove: (name) => {
+			if ((name = null)) {
+				return [`Session name?`, PROB];
+			}
+			if (!s_idxs.includes(name)) {
+				return [`Session "${name}" not found`, BAD];
+			}
+			if (s_idxs.length <= 1) {
+				return [`There has to be AT LEAST one session!`, PROB];
+			}
 
-        return [`Downloaded all sessions as "sessions_${time}.json"`, GOOD];
-    },
+			current_session = 0;
+			all_times = getSession();
 
-    // import session
-    "import": () => {
-        localStorage.session_backup = localStorage.sessions;
+			delete sessions[name];
+			s_idxs[s_idxs.indexOf(name)] = null;
+			s_idxs = s_idxs.filter((i) => i);
 
-        var input = upload();
+			// session select
+			s_length = s_idxs.length;
+			sessionSelect.innerHTML = "";
+			createSelect(s_length, s_idxs, sessionSelect, (i, opt) => {
+				return i == current_session;
+			});
 
-        input.onchange = (e) => {
-            let file = e.target.files[0];
+			logs();
 
-            var reader = new FileReader();
-            reader.readAsText(file, 'UTF-8');
+			return [`Removed session: ${name}`, PROB];
+		},
 
-            reader.onload = (readerEvent) => {
-                content = readerEvent.target.result;
+		// rename a session
+		// TODO
+	},
 
-                try {
-                    json = JSON.parse(content);
-                    console.log(json);
+	// export session
+	export: () => {
+		var now = new Date();
+		var time =
+			`${now.getFullYear()}${now.getMonth() + 1}${now.getDate()}_` +
+			`${now.getHours()}${now.getMinutes()}${now.getSeconds()}`;
 
-                    sessions = json;
-                    s_idxs = Object.keys(sessions);
-                    s_length = s_idxs.length;
-                    current_session = 0;
+		download("application/json", JSON.stringify(sessions), `sessions_${time}`);
 
-                    all_times = getSession();
+		return [`Downloaded all sessions as "sessions_${time}.json"`, GOOD];
+	},
 
-                    logs();
+	// import session
+	import: () => {
+		localStorage.session_backup = localStorage.sessions;
 
-                    set_status(`Imported!`, GOOD);
+		var input = upload();
 
-                } catch (E) {
-                    set_status(`Invalid file!`, BAD);
-                }
-            }
-        }
-    },
+		input.onchange = (e) => {
+			let file = e.target.files[0];
 
-    // restore sessions from backup
-    "restore": async () => {
-        localStorage.sessions = localStorage.session_backup;
+			var reader = new FileReader();
+			reader.readAsText(file, "UTF-8");
 
-        location.href = location.href;
-    },
+			reader.onload = (readerEvent) => {
+				content = readerEvent.target.result;
+
+				try {
+					json = JSON.parse(content);
+					console.log(json);
+
+					sessions = json;
+					s_idxs = Object.keys(sessions);
+					s_length = s_idxs.length;
+					current_session = 0;
+
+					all_times = getSession();
+
+					logs();
+
+					set_status(`Imported!`, GOOD);
+				} catch (E) {
+					set_status(`Invalid file!`, BAD);
+				}
+			};
+		};
+	},
+
+	// restore sessions from backup
+	restore: async () => {
+		localStorage.sessions = localStorage.session_backup;
+
+		location.href = location.href;
+	},
 };
 
 const unknown = "unknown";
-var list     = ["cube", "scramble", "time", "punish", "date", "comment", "tags"];
-var defaults = [unknown, "freedom", unknown, null, unknown, "",  []];
+var list = ["cube", "scramble", "time", "punish", "date", "comment", "tags"];
+var defaults = [unknown, "freedom", unknown, null, unknown, "", []];
 
 const $other_commands = {
-    "version": () => { return [`last update: ${last_update}`, GOOD] },
-    "refresh": () => { location.href = location.href; },
-    "reload": "refresh",
+	version: () => {
+		return [`last update: ${last_update}`, GOOD];
+	},
+	refresh: () => {
+		location.href = location.href;
+	},
+	reload: "refresh",
 
-    "update": () => {
-        s_idxs.forEach((name) => {
-            console.log(name);
-            JSON.parse(sessions[name]).forEach((item, idx, arr) => {
-                console.log(item);
+	update: () => {
+		s_idxs.forEach((name) => {
+			console.log(name);
+			JSON.parse(sessions[name]).forEach((item, idx, arr) => {
+				console.log(item);
 
-                list.forEach((elem, i) => {
-                    if (!item[elem]) {
-                        item[elem] = defaults[i];
-                    }
-                });
+				list.forEach((elem, i) => {
+					if (!item[elem]) {
+						item[elem] = defaults[i];
+					}
+				});
 
-                arr[idx] = item;
-            })
-        })
-    },
+				arr[idx] = item;
+			});
+		});
+	},
 
-    "reset": () => {
-       localStorage.clear();
+	reset: () => {
+		localStorage.clear();
 
-        // refresh the page
-        location.href = location.href;
-    },
+		// refresh the page
+		location.href = location.href;
+	},
 
-    "help": () => {
-        window.open("help.html", "_blank");
-    },
+	help: () => {
+		window.open("help.html", "_blank");
+	},
 
-    "test": {
-        null: () => { console.log("default") },
-        "hi": () => { console.log("hello") },
-    },
+	test: {
+		null: () => {
+			console.log("default");
+		},
+		hi: () => {
+			console.log("hello");
+		},
+	},
 };
 
 const $commands = mergeObjects($cube_commands, $other_commands);
